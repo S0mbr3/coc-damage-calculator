9e8c23e0b93efce8870a325c649f35a61008a43b
Benjamin <benjamin_denis@outlook.com>
2024-10-03 04:46:29 -0300

    implement earthquake-calculator function

---
+++ fireball.el
@@ -0,0 +1,18 @@
+;;; packagge -- Summary
+;;; Commentary:
+;;; -*- lexical-binding: t; -*-
+;;; Code:
+
+(defun earthquake-calculator (number damage)
+  "Calculate earthquake damage depending of NUMBER DAMAGE."
+  (let* (( i 2)
+	 (first damage)
+	 (total first))
+    (while (<= i number)
+      (setq total (+ total (/ first (- (* 2 i) 1))))
+      (setq i (1+ i)))
+    total)
+  )
+(earthquake-calculator 3 1711.0)
+(provide 'fireball)
+;;; fireball.el ends here
