6c82bee87dfd2f7d1955c08ffe417557a4079dac
Benjamin <benjamin_denis@outlook.com>
2024-10-08 16:40:03 -0300

    fixed: version and summary

--- coc-damage-calculator.el
+++ coc-damage-calculator.el
@@ -1,7 +1,29 @@
-;;; coc-damage-calculator.el -- a damage calculator for various equipements, spells  of clash of clans
-;;; -*- lexical-binding: t; -*-
+;;; coc-damage-calculator.el -- A Clash of Clans damage calculator -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Benjamin Denis
+
+;; Author: Benjamin Denis <benjamin_denis@outlook.com>
+;; Keywords: coc damage calculator
+;; Homepage: https://github.com/S0mbr3/coc-damage-calculator
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.2") (hydra "0.14.0")
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;; This package will help finding good combinations of spells, equipements to deal damage to buildings in clash of clans.
+;; This package help finding good combinations of spells, equipements to deal damage to buildings in Clash of Clans.
+
 ;;; Code:
 
 (defun earthquake-level-picker (level)
