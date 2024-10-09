8092675edda3d75e33d6159b174704739842fb43
Benjamin <benjamin_denis@outlook.com>
2024-10-09 13:47:39 -0300

    add autoload directive to functions

--- coc-damage-calculator.el
+++ coc-damage-calculator.el
@@ -26,11 +26,13 @@
 
 ;;; Code:
 
+;;;###autoload
 (defun earthquake-level-picker (level)
   "Pick the good percentage for the earthquake LEVEL."
   (let ((levels '(14.5 17 21 25 29)))
     (elt levels (1- level))))
 
+;;;###autoload
 (defun earthquake-damage-calculator (level number hp)
   "Calculate earthquake damage depending of NUMBER LEVEL and HP."
   (let* (( i 2)
@@ -43,25 +45,35 @@
       (setq i (1+ i)))
     total))
 
+;;;###autoload
 (defun earthquake-hp-building-left (level number hp)
   "Calculate remaining building HP after NUMBER earthquakes of some LEVEL."
   (interactive "nPress the level of the earthquake:\nnPress the number of earthquakes:\nnPress the hp of the building:")
   (print (format "earthquakes building hp left: %d" (- hp(earthquake-damage-calculator level number hp)))))
 
+;;;###autoload
 (defun fireball-level-picker (level)
   "Pick the good damage for the fireball LEVEL."
   (let ((levels '(1500 1500 1700 1700 1800 1950 1950 2050 2200 2200 2350 2650 2650 2750 3100 3100 3250 3400 3400 3500 3650 3650 3750 3900 3900 3950 4100))) (elt levels (1- level))))
 
 
+;;;###autoload
 (defun fireball-hp-building-left (level hp)
   "Calculate fireball damage by it's LEVEL building HP."
   (interactive "nPress the level of the fireball: \nnPress the hp of the building: ")
   (print (format "fireball building hp left: %d" (- hp (fireball-level-picker level)))))
 
+;;;###autoload
 (defun fireball-and-earthquake-calculator (fireball-level earthquake-level earthquake-number hp)
   "Calculate damage FIREBALL-LEVEL EARTHQUAKE-LEVEL EARTHQUAKE-NUMBER HP."
   (interactive "nPress the level of the fireball: \nnPress the level of the earthquake: \nnPress the number of earthquakes: \nnPress the hp of the building: ")
   (print (- hp (fireball-level-picker fireball-level ) (earthquake-damage-calculator earthquake-level earthquake-number hp))))
+
+;;;###autoload
+(defun rocket-spear-level-picker (level)
+  "Pick the good percentage for the earthquake LEVEL."
+  (let ((levels '(14.5 17 21 25 29)))
+    (elt levels (1- level))))
 
 (provide 'coc-damage-calculator)
 ;;; coc-damage-calculator.el ends here
